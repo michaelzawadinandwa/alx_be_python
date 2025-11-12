646eab3322e9b4002c9b85dad01d7d3d2861f0ef
michaelzawadinandwa <michaelnandwa13@gmail.com>
2025-11-12 22:09:53 +0300

    control_flow

--- control_flow/weather_advice.py
+++ control_flow/weather_advice.py
@@ -6,7 +6,7 @@ time_bound = input("Is it time-bound? (yes/no): ").lower()
 match priority:
     case "high":
         if time_bound == "yes":
-            print(f"Reminder: '{task}' is a high priority task that requires immediate attention immediately !")
+            print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
         else:
             print(f"Reminder: '{task}' is a high priority task. Try to complete it soon.")
     case "medium":
